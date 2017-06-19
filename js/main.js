a12e0a7bb5acf77e956e711727ac8213747df01c
Maribel Diaz <maribel.diaz.j@gmail.com>
2017-06-18 23:51:36 -0300

    intento de js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 /*aquí va tu código*/
+
+/* LocalStorage: Guarda información que permanecerá almacenada por tiempo indefinido; sin importar que el navegador se cierre.*/
+
+function agregarComentario(){
+	var comentario = document.getElemetById("comentario");
+	var nombre = document.getElemetById("nombre").value;
+	var contenido = document..getElemetById("valor").value;
+}
+
+
+
+
+
+
+
+
+
+
+/*function agregarComentario(){
+	localStorage.nombre = document.getElemetById("nombre").value;
+	localStorage.comentario = document.getElemetById("comentario").value;
+}
+
+function limpiarComentario(){
+	if ((localStorage.nombre != undefined) && (localStorage.comentario != undefined)){
+		document.getElemetById("datos").innerHTML = "Nombre: " + localStorage.nombre + "<br/> comentario: " + localStorage.comentario;
+}
+else{
+	document.getElemetById("datos").innerHTML = "No tienes comentarios ingresados";
+  }
+}*/
